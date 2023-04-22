192550af080118377a599d51bdac9d0c1e7d64be
Parv Desai <pdesai@splunk.com>
2023-04-22 14:50:24 +0530

    Configured database, added schemas

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+env_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_NAME:str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Settings()
