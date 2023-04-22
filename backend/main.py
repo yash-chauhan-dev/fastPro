aba49cb8ab7aa6c4809817aa4c5418b9d74783aa
Parv Desai <pdesai@splunk.com>
2023-04-22 12:38:48 +0530

    Add support for jinja template response in homepage

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+def include_router(app):
+    app.include_router(general_pages_router)
 
-@app.get('/')
-def hello_api():
-    return {"msg":"Hello API"}
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
+
+app = start_application()
