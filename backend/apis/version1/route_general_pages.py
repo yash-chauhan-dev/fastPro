f1831c40870eb82b38ceed846016dd37a1dae14b
Parv Desai <pdesai@splunk.com>
2023-04-22 16:38:22 +0530

    support user creation using post

--- backend/apis/version1/route_general_pages.py
+++ backend/apis/version1/route_general_pages.py
@@ -6,6 +6,7 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 general_pages_router = APIRouter()
 
+
 @general_pages_router.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("general_pages/homepage.html", {"request":request})
+    return templates.TemplateResponse("general_pages/homepage.html", {"request": request})
--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,30 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
-from apis.general_pages.route_homepage import general_pages_router
+from apis.base import api_router
 from db.session import engine
 from db.base import Base
 
+
 def include_router(app):
-    app.include_router(general_pages_router)
+    app.include_router(api_router)
+
 
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_NAME,
+                  version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
     create_tables()
     return app
+
 
 app = start_application()
